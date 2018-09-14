3e01a062c6f3600b3d8411edd40c131c7fc648f0
KaiChen <237745537@qq.com>
2018-09-14 16:16:39 +0800

    add hello world

---
+++ src/main/java/com/chk/md/web/HelloController.java
@@ -0,0 +1,18 @@
+package com.chk.md.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:kaichenr
+ * @Date:2018/9/14 16:10
+ * @Description:
+ **/
+@RestController("/")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
